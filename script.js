a0886aad8773964ec62309eac206b61d4cca1929
Michael <michael4sure20@gmail.com>
2019-07-18 09:32:20 +0100

    Readjusting the button

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-function todoList(){
+function todoList(e){
+    e.preventDefault();
     var item = document.getElementById("todoInput").value;
     var text = document.createTextNode(item);
     var newItem = document.createElement("li");
     newItem.appendChild(text);
     document.getElementById("todoList").appendChild(newItem);
+    document.getElementById("todoInput").value=""
 }
